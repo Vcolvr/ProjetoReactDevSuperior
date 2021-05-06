97fe776eb7913d1049122e0666eb6d639870fedd
Vitor <vcolvr@gmail.com>
2021-05-06 19:51:33 -0300

    Componentes estáticos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/vcolvr" target="_blank" rel="noreferrer">Vitor Carvalho de Oliveira</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Entre em contato pelo meu LinkedIn<a href="https://www.linkedin.com/in/vitor-carvalho-916b1118a/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
